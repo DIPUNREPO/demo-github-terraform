1d23b861165a09d2eb7cf09eaa9a051003468f74
dipun <skpattanayak77@gmail.com>
2023-06-26 18:46:42 +0000

    github action pipeline13

--- backend.tf
+++ backend.tf
@@ -2,6 +2,8 @@ terraform {
   backend "s3" {
     bucket = "github-action3-demo"
     key    = "github-action3-demo.tfstate"
+    access_key = "AKIAUOLSNUJOZYN4E7FC"
+    secret_key = "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"
     region = "us-east-2"
   }
 }
