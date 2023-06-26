1e3292ff6327ece439932b350b106859e36f6677
dipun <skpattanayak77@gmail.com>
2023-06-26 15:37:22 +0000

    my comit

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+
+  region = "us-east-2"
+
+}
+
+resource "aws_instance" "mygitserver" {
+  ami           = "ami-0e820afa569e84cc1"
+  key_name = "dibya_ohio_keypair"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "mygitserver"
+  }
+}
