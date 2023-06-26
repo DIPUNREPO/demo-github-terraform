9ffd87be5545760f45fa3a12f22544de29aacd34
dipun <skpattanayak77@gmail.com>
2023-06-26 18:43:33 +0000

    github action pipeline12

--- main.tf
+++ main.tf
@@ -1,10 +1,3 @@
-provider "aws" {
-
-  region = "us-east-2"
-  access_key = "AKIAUOLSNUJOZYN4E7FC"
-  secret_key = "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"
-
-}
 
 resource "aws_instance" "mygitserver" {
   ami           = "ami-0e820afa569e84cc1"
